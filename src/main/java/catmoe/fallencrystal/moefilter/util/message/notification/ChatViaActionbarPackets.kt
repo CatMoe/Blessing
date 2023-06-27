77a74a956ba83035be1062d5a8d7a2389c2cdbe0
Akkariin <my13794531826@Outlook.com>
2023-06-27 21:23:10 +0800

    Fix notifications actionbar packet send bugs (f u mojang)

--- src/main/java/catmoe/fallencrystal/moefilter/util/message/notification/ChatViaActionbarPackets.kt
+++ src/main/java/catmoe/fallencrystal/moefilter/util/message/notification/ChatViaActionbarPackets.kt
@@ -1,6 +1,7 @@
 package catmoe.fallencrystal.moefilter.util.message.notification
 
+import net.md_5.bungee.protocol.packet.Chat
 import net.md_5.bungee.protocol.packet.SystemChat
 import net.md_5.bungee.protocol.packet.Title
 
-class ChatViaActionbarPackets(val v117: SystemChat?, val v111: Title?, val v110: SystemChat?, val has117data: Boolean, val has111data: Boolean, val has110data: Boolean)
+class ChatViaActionbarPackets(val v119: SystemChat?, val v117: Chat?, val v111: Title?, val v110: Chat?, val has119data: Boolean, val has117data: Boolean, val has111data: Boolean, val has110data: Boolean)
