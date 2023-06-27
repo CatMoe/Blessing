e5ef9d5afd336758a15258f67429b514764a65b3
Akkariin <my13794531826@Outlook.com>
2023-06-27 19:39:58 +0800

    Use abstract class instead of write same codes. (#24)

--- src/main/java/catmoe/fallencrystal/moefilter/network/bungee/pipeline/BungeePipeline.kt
+++ src/main/java/catmoe/fallencrystal/moefilter/network/bungee/pipeline/BungeePipeline.kt
@@ -1,92 +1,9 @@
 package catmoe.fallencrystal.moefilter.network.bungee.pipeline
 
-import catmoe.fallencrystal.moefilter.api.proxy.ProxyCache
-import catmoe.fallencrystal.moefilter.common.utils.counter.ConnectionCounter
-import catmoe.fallencrystal.moefilter.listener.firewall.FirewallCache
-import catmoe.fallencrystal.moefilter.listener.firewall.Throttler
-import catmoe.fallencrystal.moefilter.network.bungee.decoder.VarIntFrameDecoder
-import catmoe.fallencrystal.moefilter.network.bungee.handler.InboundHandler
-import catmoe.fallencrystal.moefilter.network.bungee.handler.MoeInitialHandler
-import catmoe.fallencrystal.moefilter.network.bungee.handler.TimeoutHandler
-import catmoe.fallencrystal.moefilter.network.bungee.pipeline.geyser.GeyserPipeline
-import catmoe.fallencrystal.moefilter.network.bungee.util.ExceptionCatcher
-import catmoe.fallencrystal.moefilter.network.bungee.util.event.EventCallMode
-import catmoe.fallencrystal.moefilter.network.bungee.util.event.EventCaller
-import io.netty.channel.Channel
 import io.netty.channel.ChannelHandlerContext
-import io.netty.channel.ChannelInitializer
-import io.netty.channel.ChannelOption
-import io.netty.handler.codec.haproxy.HAProxyMessageDecoder
 import lombok.RequiredArgsConstructor
-import net.md_5.bungee.BungeeCord
-import net.md_5.bungee.netty.PipelineUtils
-import net.md_5.bungee.protocol.*
-import java.net.InetSocketAddress
 
 @RequiredArgsConstructor
-class BungeePipeline : ChannelInitializer<Channel>(), IPipeline {
-    private val bungee = BungeeCord.getInstance()
-    private val throttler = bungee.connectionThrottle
-    private val legacyKicker = KickStringWriter()
-    private val protocol = 0
-
-    @Throws(Exception::class)
-    override fun initChannel(channel: Channel) {
-        val parent = channel.parent()
-        val isGeyser = parent != null && parent.javaClass.canonicalName.startsWith("org.geysermc.geyser")
-        if (isGeyser) { GeyserPipeline().handle(channel, protocol) }
-    }
-
-    @Throws(Exception::class)
-    override fun handlerAdded(ctx: ChannelHandlerContext) {
-        try {
-            val channel = ctx.channel()
-            val remoteAddress = if (channel.remoteAddress() == null) channel.parent().localAddress() else channel.remoteAddress()
-            val inetAddress = (remoteAddress as InetSocketAddress).address
-            val pipeline = channel.pipeline()
-            val listener = channel.attr(PipelineUtils.LISTENER).get()
-            val eventCaller = EventCaller(ctx, listener)
-
-            ConnectionCounter.increase(inetAddress)
-            eventCaller.call(EventCallMode.AFTER_INIT)
-            if (FirewallCache.isFirewalled(inetAddress)) { channel.close(); return }
-            eventCaller.call(EventCallMode.NON_FIREWALL)
-            if (Throttler.increase(inetAddress)) { channel.close(); return }
-            if (throttler != null && throttler.throttle(remoteAddress)) { channel.close(); return }
-            eventCaller.call(EventCallMode.READY_DECODING)
-
-            if (ProxyCache.isProxy(inetAddress)) { FirewallCache.addAddress(inetAddress, true); channel.close(); return }
-            MoeChannelHandler.register(pipeline)
-            PipelineUtils.BASE.initChannel(channel)
-
-            // MoeFilter has VarIntFrameDecoder, TimeoutHandler and InboundHandler itself.
-            pipeline.replace(PipelineUtils.FRAME_DECODER, PipelineUtils.FRAME_DECODER, VarIntFrameDecoder())
-            // like https://github.com/PaperMC/Waterfall/commit/6702e0f69b2fa32c1046d277ade2107e22ba9134
-            pipeline.replace(PipelineUtils.TIMEOUT_HANDLER, PipelineUtils.TIMEOUT_HANDLER, TimeoutHandler(bungee.getConfig().timeout.toLong()))
-            pipeline.replace(PipelineUtils.BOSS_HANDLER, PipelineUtils.BOSS_HANDLER, InboundHandler())
-
-            // Init default bungeecord pipeline
-            pipeline.addBefore(PipelineUtils.FRAME_DECODER, PipelineUtils.LEGACY_DECODER, LegacyDecoder())
-            pipeline.addAfter(PipelineUtils.FRAME_DECODER, PipelineUtils.PACKET_DECODER, MinecraftDecoder(Protocol.HANDSHAKE, true, protocol))
-            pipeline.addAfter(PipelineUtils.FRAME_PREPENDER, PipelineUtils.PACKET_ENCODER, MinecraftEncoder(Protocol.HANDSHAKE, true, protocol))
-            pipeline.addBefore(PipelineUtils.FRAME_PREPENDER, PipelineUtils.LEGACY_KICKER, legacyKicker)
-
-            // MoeFilter -- TND default should be true.
-            channel.config().setOption(ChannelOption.TCP_NODELAY, true)
-
-            // MoeFilter's InitialHandler
-            pipeline.get(InboundHandler::class.java).setHandler(MoeInitialHandler(ctx, listener))
-
-            if (listener.isProxyProtocol) pipeline.addFirst(HAProxyMessageDecoder())
-
-            eventCaller.call(EventCallMode.AFTER_DECODER)
-        } finally { if (!ctx.isRemoved) { ctx.pipeline().remove(this) } }
-    }
-
-    override fun handlerRemoved(ctx: ChannelHandlerContext?) {
-        //
-    }
-
-    @Suppress("OVERRIDE_DEPRECATION")
-    override fun exceptionCaught(ctx: ChannelHandlerContext?, cause: Throwable?) { ExceptionCatcher.handle(ctx!!.channel(), cause!!) }
+class BungeePipeline : AbstractPipeline(), IPipeline {
+    override fun handlerAdded(ctx: ChannelHandlerContext) { try { super.handlerAdded(ctx) } finally { if (!ctx.isRemoved) { ctx.pipeline().remove(this) } } }
 }
--- src/main/java/catmoe/fallencrystal/moefilter/network/bungee/pipeline/botfilter/BotFilterPipeline.kt
+++ src/main/java/catmoe/fallencrystal/moefilter/network/bungee/pipeline/botfilter/BotFilterPipeline.kt
@@ -1,86 +1,20 @@
 package catmoe.fallencrystal.moefilter.network.bungee.pipeline.botfilter
 
-import catmoe.fallencrystal.moefilter.api.proxy.ProxyCache
-import catmoe.fallencrystal.moefilter.common.utils.counter.ConnectionCounter
-import catmoe.fallencrystal.moefilter.listener.firewall.FirewallCache
-import catmoe.fallencrystal.moefilter.listener.firewall.Throttler
-import catmoe.fallencrystal.moefilter.network.bungee.handler.InboundHandler
-import catmoe.fallencrystal.moefilter.network.bungee.handler.MoeInitialHandler
-import catmoe.fallencrystal.moefilter.network.bungee.handler.TimeoutHandler
+import catmoe.fallencrystal.moefilter.network.bungee.pipeline.AbstractPipeline
 import catmoe.fallencrystal.moefilter.network.bungee.pipeline.IPipeline
-import catmoe.fallencrystal.moefilter.network.bungee.pipeline.MoeChannelHandler
-import catmoe.fallencrystal.moefilter.network.bungee.pipeline.geyser.GeyserPipeline
-import catmoe.fallencrystal.moefilter.network.bungee.util.ExceptionCatcher
-import catmoe.fallencrystal.moefilter.network.bungee.util.event.EventCallMode
-import catmoe.fallencrystal.moefilter.network.bungee.util.event.EventCaller
-import io.netty.channel.Channel
 import io.netty.channel.ChannelHandlerContext
-import io.netty.channel.ChannelInitializer
-import io.netty.handler.codec.haproxy.HAProxyMessageDecoder
 import lombok.RequiredArgsConstructor
-import net.md_5.bungee.BungeeCord
 import net.md_5.bungee.netty.PipelineUtils
-import net.md_5.bungee.protocol.*
-import java.net.InetSocketAddress
+import net.md_5.bungee.protocol.Varint21FrameDecoder
 
 @RequiredArgsConstructor
-class BotFilterPipeline : ChannelInitializer<Channel>(), IPipeline {
-    private val bungee = BungeeCord.getInstance()
-    private val throttler = bungee.connectionThrottle
-    private val protocol = 0
-    private val legacyKicker = KickStringWriter()
-
-    @Throws(Exception::class)
-    override fun initChannel(channel: Channel) {
-        val parent = channel.parent()
-        val isGeyser = parent != null && parent.javaClass.canonicalName.startsWith("org.geysermc.geyser")
-        if (isGeyser) { GeyserPipeline().handle(channel, protocol) }
-    }
+class BotFilterPipeline : AbstractPipeline(), IPipeline {
 
     override fun handlerAdded(ctx: ChannelHandlerContext) {
         try {
-            val channel = ctx.channel()
-            val remoteAddress = if (channel.remoteAddress() == null) channel.parent().localAddress() else channel.remoteAddress()
-            val inetAddress = (remoteAddress as InetSocketAddress).address
-            val pipeline = channel.pipeline()
-            val listener = channel.attr(PipelineUtils.LISTENER).get()
-            val eventCaller = EventCaller(ctx, listener)
-
-            ConnectionCounter.increase(inetAddress)
-            eventCaller.call(EventCallMode.AFTER_INIT)
-            if (FirewallCache.isFirewalled(inetAddress)) { channel.close(); return }
-            eventCaller.call(EventCallMode.NON_FIREWALL)
-            if (Throttler.increase(inetAddress)) { channel.close(); return }
-            if (throttler != null && throttler.throttle(remoteAddress)) { channel.close(); return }
-            eventCaller.call(EventCallMode.READY_DECODING)
-
-            if (ProxyCache.isProxy(inetAddress)) { FirewallCache.addAddress(inetAddress, true); channel.close(); return }
-            MoeChannelHandler.register(pipeline)
-            PipelineUtils.BASE.initChannel(channel)
-
-            // VarIntFameDecoder exploit is safe for BotFilter.
-            // pipeline.replace(PipelineUtils.FRAME_DECODER, PipelineUtils.FRAME_DECODER, VarIntFrameDecoder())
-            pipeline.replace(PipelineUtils.TIMEOUT_HANDLER, PipelineUtils.TIMEOUT_HANDLER, TimeoutHandler(bungee.getConfig().timeout.toLong()))
-            pipeline.replace(PipelineUtils.BOSS_HANDLER, PipelineUtils.BOSS_HANDLER, InboundHandler())
-
-            pipeline.addBefore(PipelineUtils.FRAME_DECODER, PipelineUtils.LEGACY_DECODER, LegacyDecoder())
-            pipeline.addAfter(PipelineUtils.FRAME_DECODER, PipelineUtils.PACKET_DECODER, MinecraftDecoder(Protocol.HANDSHAKE, true, protocol))
-            pipeline.addAfter(PipelineUtils.FRAME_PREPENDER, PipelineUtils.PACKET_ENCODER, MinecraftEncoder(Protocol.HANDSHAKE, true, protocol))
-            pipeline.addBefore(PipelineUtils.FRAME_PREPENDER, PipelineUtils.LEGACY_KICKER, legacyKicker)
-
-            // TCP NO DELAY is always enabled on BotFilter/Waterfall
-
-            // MoeFilter's InitialHandler
-            pipeline.get(InboundHandler::class.java).setHandler(MoeInitialHandler(ctx, listener))
-
-            if (listener.isProxyProtocol) pipeline.addFirst(HAProxyMessageDecoder())
-
-            eventCaller.call(EventCallMode.AFTER_DECODER)
+            // Use original Varint21FrameDecoder for BotFilter.
+            super.handlerAdded(ctx)
+            ctx.pipeline().replace(PipelineUtils.FRAME_DECODER, PipelineUtils.FRAME_DECODER, Varint21FrameDecoder())
         } finally { if (!ctx.isRemoved) { ctx.pipeline().remove(this) } }
     }
-
-    override fun handlerRemoved(ctx: ChannelHandlerContext) { /* Ignored */ }
-
-    @Suppress("OVERRIDE_DEPRECATION")
-    override fun exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable) { ExceptionCatcher.handle(ctx.channel(), cause) }
 }
