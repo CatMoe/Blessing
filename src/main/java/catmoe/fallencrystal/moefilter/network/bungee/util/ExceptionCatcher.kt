49b0c13ec659715a0bfa0827795d6ac84de9ea51
Akkariin <71176602+FallenCrystal@users.noreply.github.com>
2023-07-06 22:22:30 +0800

    Update ExceptionCatcher.kt

--- src/main/java/catmoe/fallencrystal/moefilter/network/bungee/util/ExceptionCatcher.kt
+++ src/main/java/catmoe/fallencrystal/moefilter/network/bungee/util/ExceptionCatcher.kt
@@ -1,10 +1,12 @@
 package catmoe.fallencrystal.moefilter.network.bungee.util
 
 import catmoe.fallencrystal.moefilter.common.config.LocalConfig
+import catmoe.fallencrystal.moefilter.util.message.v2.MessageUtil
 import catmoe.fallencrystal.moefilter.listener.firewall.FirewallCache
 import catmoe.fallencrystal.moefilter.network.bungee.util.exception.DebugException
 import catmoe.fallencrystal.moefilter.network.bungee.util.exception.InvalidHandshakeStatusException
 import catmoe.fallencrystal.moefilter.network.bungee.util.exception.InvalidStatusPingException
+import com.typesafe.config.ConfigException
 import io.netty.channel.Channel
 import java.io.IOException
 import java.net.InetSocketAddress
@@ -19,6 +21,7 @@ object ExceptionCatcher {
         if (cause is IOException) return
         if (cause is DebugException) { cause.printStackTrace(); return }
         if (cause is InvalidStatusPingException || cause is InvalidHandshakeStatusException) { FirewallCache.addAddress(address, true); return }
+        if (cause is ConfigException) { MessageUtil.logError("<red>A connection force closed because your config has critical issue"); cause.printStackTrace(); return }
         FirewallCache.addAddressTemp(address, true)
     }
 
