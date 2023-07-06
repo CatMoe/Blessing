5aa069e2c8c8ecd5f30e75a7d1fda6bd0ccd2345
Akkariin <71176602+FallenCrystal@users.noreply.github.com>
2023-07-06 09:07:18 +0800

    Update ComponentUtil.kt

--- src/main/java/catmoe/fallencrystal/moefilter/util/message/component/ComponentUtil.kt
+++ src/main/java/catmoe/fallencrystal/moefilter/util/message/component/ComponentUtil.kt
@@ -2,6 +2,7 @@ package catmoe.fallencrystal.moefilter.util.message.component
 
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.minimessage.MiniMessage
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
 import net.kyori.adventure.text.serializer.gson.GsonComponentSerializer
 import net.md_5.bungee.api.chat.BaseComponent
 import net.md_5.bungee.api.chat.TextComponent
@@ -9,6 +10,10 @@ import net.md_5.bungee.chat.ComponentSerializer
 
 object ComponentUtil {
     fun toBaseComponents(component: Component): BaseComponent { return TextComponent(*ComponentSerializer.parse(toGson(component))) }
+
+    fun legacyToComponent(legacy: String): Component { return LegacyComponentSerializer.legacySection().deserialize(ChatColor.translateAlternateColorCodes('&', legacy)) as Component }
+
+    fun ComponentToRaw(component: Component): String { return MiniMessage.builder().strict(true).build().serialize(component) }
 
     private fun toGson(component: Component): String { return GsonComponentSerializer.gson().serialize(component) }
 
