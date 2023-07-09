f75e4628ea7d110dbcd6f0e838a005f6d76c5b66
Akkariin <71176602+FallenCrystal@users.noreply.github.com>
2023-07-10 05:35:40 +0800

    Update AddressCheck.kt

--- src/main/java/catmoe/fallencrystal/moefilter/common/check/info/impl/AddressCheck.kt
+++ src/main/java/catmoe/fallencrystal/moefilter/common/check/info/impl/AddressCheck.kt
@@ -1,5 +1,6 @@
 package catmoe.fallencrystal.moefilter.common.check.info.impl
 
 import catmoe.fallencrystal.moefilter.common.check.info.CheckInfo
+import java.net.InetSocketAddress
 
 class AddressCheck(val address: InetSocketAddress) : CheckInfo
