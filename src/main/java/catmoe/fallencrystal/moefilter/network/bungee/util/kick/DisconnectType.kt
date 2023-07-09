ec529758376aca4245372effcfe0d05b90603f67
Akkariin <71176602+FallenCrystal@users.noreply.github.com>
2023-07-10 05:50:53 +0800

    Update DisconnectType.kt

--- src/main/java/catmoe/fallencrystal/moefilter/network/bungee/util/kick/DisconnectType.kt
+++ src/main/java/catmoe/fallencrystal/moefilter/network/bungee/util/kick/DisconnectType.kt
@@ -4,5 +4,6 @@ enum class DisconnectType(@JvmField val messagePath: String) {
     ALREADY_ONLINE("kick.already-online"),
     REJOIN("kick.rejoin"),
     PING("kick.ping"),
-    INVALID_NAME("kick.invalid-name")
+    INVALID_NAME("kick.invalid-name"),
+    INVALID_HOST("kick.invalid-host"),
 }
