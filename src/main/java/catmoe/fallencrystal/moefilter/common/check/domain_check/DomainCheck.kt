b643b0f8a9cbaee718aa83496b81304bd1400c17
Akkariin <71176602+FallenCrystal@users.noreply.github.com>
2023-07-10 10:07:00 +0800

    [ci skip] (Improve) [DomainCheck] Use cache instead of forEach

--- src/main/java/catmoe/fallencrystal/moefilter/common/check/domain_check/DomainCheck.kt
+++ src/main/java/catmoe/fallencrystal/moefilter/common/check/domain_check/DomainCheck.kt
@@ -1,18 +1,26 @@
 package catmoe.fallencrystal.moefilter.common.check.domain_check
 
 import java.util.concurrent.atomic.AtomicBoolean
+import com.github.benmanes.caffeine.cache.Caffeine
 import catmoe.fallencrystal.moefilter.common.config.LocalConfig
 import catmoe.fallencrystal.moefilter.common.check.AbstractCheck
 import catmoe.fallencrystal.moefilter.common.check.info.CheckInfo
 import catmoe.fallencrystal.moefilter.common.check.info.impl.AddressCheck
 
-class DomainCheck : AbstractCheck() {
+object DomainCheck : AbstractCheck() {
+
+   private val cache = Caffeine.newBuilder().build<String, Boolean>()
+   private var enable = false
+   
    override fun increase(info: CheckInfo): Boolean {
-     val config = LocalConfig.getConfig()
-     val host = (info as AddressCheck).address.hostString
-     if (!config.getBoolean("domain-check.enabled")) { return true }
-     val allowed = AtomicBoolean(false)
-     config.getStringList("domain-check.allow-lists").forEach { if (it.lowercase().equals(host.lowercase())) { allowed.set(true) } }
-     return allowed.get()
+     if (!enable) { return true }
+     return cache.getIfPresent((info as AddressCheck).address.hostString.lowercase())
+   }
+
+   fun init() {
+      cache.invalidateAll()
+      val config = LocalConfig.getConfig().getConfig("domain-check")
+      enable = config.getBoolean("enabled")
+      config.getStringList("domain-check.allow-lists").forEach { cache.put(it.lowercase(), true) }
    }
 }
