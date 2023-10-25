d6bc3e4a2becb68503da3036b2798118c5c3fde3
Akkariin <fallencrystal@outlook.com>
2023-10-25 16:06:29 +0800

    Update license and clean code

--- common/src/main/kotlin/catmoe/fallencrystal/moefilter/network/common/decoder/VarIntByteDecoder.kt
+++ common/src/main/kotlin/catmoe/fallencrystal/moefilter/network/common/decoder/VarIntByteDecoder.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2023 Velocity Contributors
+ * Copyright (C) 2023-2023. CatMoe / MoeFilter Contributors
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- translation/src/main/kotlin/catmoe/fallencrystal/translation/player/TranslatePlayer.kt
+++ translation/src/main/kotlin/catmoe/fallencrystal/translation/player/TranslatePlayer.kt
@@ -1,96 +1,23 @@
 /*
- * Copyright 2023. CatMoe / FallenCrystal
+ * Copyright (C) 2023-2023. CatMoe / MoeFilter Contributors
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package catmoe.fallencrystal.translation.player
 
-import catmoe.fallencrystal.translation.server.PlatformServer
-import catmoe.fallencrystal.translation.server.TranslateServer
-import catmoe.fallencrystal.translation.utils.version.Version
-import io.netty.channel.Channel
-import net.kyori.adventure.text.Component
-import java.net.InetSocketAddress
-import java.net.SocketAddress
-import java.util.*
-
 // 由于类的一些问题 我们需要用一个用于兼容的类来包裹那些
 // 尽管如此 也可以通过 is 来判断是VelocityPlayer还是BungeePlayer并获取它们原本的属性.
 @Suppress("MemberVisibilityCanBePrivate")
-class TranslatePlayer(val upstream: PlatformPlayer): PlatformPlayer {
-    override fun getAddress(): SocketAddress {
-        return upstream.getAddress()
-    }
-
-    override fun virtualHost(): InetSocketAddress? {
-        return upstream.virtualHost()
-    }
-
-    override fun getBrand(): String {
-        return upstream.getBrand()
-    }
-
-    override fun getVersion(): Version {
-        return upstream.getVersion()
-    }
-
-    override fun getName(): String {
-        return upstream.getName()
-    }
-
-    override fun sendMessage(component: Component) {
-        upstream.sendMessage(component)
-    }
-
-    override fun hasPermission(permission: String): Boolean {
-        return upstream.hasPermission(permission)
-    }
-
-    override fun getUniqueId(): UUID {
-        return upstream.getUniqueId()
-    }
-
-    override fun isOnlineMode(): Boolean {
-        return upstream.isOnlineMode()
-    }
-
-    override fun isOnline(): Boolean {
-        return upstream.isOnline()
-    }
-
-    override fun disconnect() {
-        upstream.disconnect()
-    }
-
-    override fun disconnect(reason: Component) {
-        upstream.disconnect(reason)
-    }
-
-    override fun send(server: PlatformServer) {
-        upstream.send(server)
-    }
-
-    override fun sendActionbar(component: Component) {
-        upstream.sendActionbar(component)
-    }
-
-    override fun channel(): Channel {
-        return upstream.channel()
-    }
-
-    override fun getServer(): TranslateServer {
-        return upstream.getServer()
-    }
-}
+class TranslatePlayer(val upstream: PlatformPlayer): PlatformPlayer by upstream
