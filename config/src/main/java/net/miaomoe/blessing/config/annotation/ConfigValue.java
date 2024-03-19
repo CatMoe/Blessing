64044ef73c47889a297835da7862b0c2f95014cd
Akkariin <fallencrystal@outlook.com>
2024-03-19 14:47:25 +0800

    feat(config): `ConfigValue` can only be annotation to fields.

--- config/src/main/java/net/miaomoe/blessing/config/annotation/ConfigValue.java
+++ config/src/main/java/net/miaomoe/blessing/config/annotation/ConfigValue.java
@@ -19,9 +19,12 @@ package net.miaomoe.blessing.config.annotation;
 
 import org.jetbrains.annotations.NotNull;
 
+import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target(value = ElementType.FIELD)
 @Retention(value= RetentionPolicy.RUNTIME)
 public @interface ConfigValue {
     @NotNull String path() default "";
